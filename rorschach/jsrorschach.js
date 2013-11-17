0184a07d149ca73650f4ae900cedfa89405df86c
gr-temporary <gr-temporary@yandex.ru>
2013-11-17 20:50:45 +0600

    Some refactoring
    Now everything works through callbacks and prototypes

--- rorschach/jsrorschach.js
+++ rorschach/jsrorschach.js
@@ -4,62 +4,66 @@
   window.requestAnimationFrame = requestAnimationFrame;
 })();
 
-var FPS = 0;
-function collectFPS () {
-	document.title = FPS.toString()+' fps';
-	FPS = 0;
-	setTimeout(collectFPS, 1000);
+var FPSCounter = function (outputField) {
+	var fps = 0;
+	var self = this;
+	this.collect = function () {
+		if(outputField == document)
+			outputField.title = fps.toString() + ' fps';
+		else
+			outputField.innerHTML = fps.toString() + ' fps';
+		fps = 0;
+		setTimeout(self.collect, 1000);
+	}
+
+	this.addFrame = function() {
+		fps++;
+	}
+
+	this.collect();
+
+	return this;
+};
+
+var JSRorschach = function(canvas) {
+
+	this.width = canvas.width;
+	this.height = canvas.height;
+
+	this.scaleX = 1.5 / this.width;
+	this.scaleY = 1.5 / this.height;
+	this.scaleT = 0.01;
+	this.time = 0;
+
+	this.ctx = canvas.getContext("2d");
+	this.imgdata = this.ctx.getImageData(0, 0, this.width, this.height);
+	this.data = this.imgdata.data;
+
+	this.paused = false;
+
+	this.simplex = new SimplexNoise();
+
+	this.self = this;
 }
 
-var jsRorschach = {
-
-	scaleX: 0.017,
-	scaleY: 0.017,
-	scaleT: 0.01,
-	time: 0,
-
-	width: 0,
-	height: 0,
-
-	ctx: null,
-	imgdata: null,
-	data: null,
-
-	paused: false,
-
-	simplex: new SimplexNoise(),
-
-	init: function (canvas) {
-		this.width = canvas.width;
-		this.height = canvas.height;
-
-		this.scaleX = 1.5 / this.width;
-		this.scaleY = 1.5 / this.height;
-
-		this.ctx = canvas.getContext("2d");
-		this.imgdata = this.ctx.getImageData(0, 0, this.width, this.height);
-		this.data = this.imgdata.data;
-	},
-
-	step: function () {
+JSRorschach.prototype.step = function() {
+	if(this.data) {
 		this.time++;
-		var simplex = this.simplex,
+		var width = this.width,
+			height = this.height,
 			scaleX = this.scaleX,
 			scaleY = this.scaleY,
 			scaleT = this.scaleT,
-			time = this.time,
-			width = this.width,
-			height = this.height,
-			data = this.data;
+			time = this.time;
 		var i, j, x, y, n, n2, n3;
 		for(i=0; i<width / 2; i++) {
 			x = (i - width * 0.5) * (i - width * 0.5) * 4 / (width * width);
 			for(j=0; j<height; j++) {
 				y = (j - height * 0.5) * (j - height * 0.5) * 4 / (height * height);
 
-				n = simplex.noise3D(i * scaleX, j * scaleY, time * scaleT) * 0.5 + 0.5;
-				n2 = simplex.noise3D(i * scaleX * 2, j * scaleY * 2, time * scaleT * 0.7) * 0.5 + 0.5;
-				n3 = simplex.noise3D(i * scaleX * 4, j * scaleY * 4, time * scaleT * 1.1) * 0.5 + 0.5;
+				n = this.simplex.noise3D(i * scaleX, j * scaleY, time * scaleT) * 0.5 + 0.5;
+				n2 = this.simplex.noise3D(i * scaleX * 2, j * scaleY * 2, time * scaleT * 0.7) * 0.5 + 0.5;
+				n3 = this.simplex.noise3D(i * scaleX * 4, j * scaleY * 4, time * scaleT * 1.1) * 0.5 + 0.5;
 				n = (n + n2 + n3) / 3;
 				n -= Math.pow((x > y ? x : y) * 0.8, 4);
 				if(n < 0.48)
@@ -75,30 +79,37 @@ var jsRorschach = {
 				this.putPixel(width - i - 1, j, n);
 			}
 		}
-		this.ctx.putImageData(this.imgdata, 0, 0);
-	},
-
-	animate: function() {
-		if(!jsRorschach.paused)
-			requestAnimationFrame( jsRorschach.animate );
-		jsRorschach.step();
-		FPS++;
-	},
-
-	pause: function() {
-		this.paused = true;
-	},
-
-	play: function() {
-		this.paused = false;
-		this.animate();
-	},
-
-	putPixel: function (x, y, v) {
-		var p = (x + y * this.width) << 2;
-		this.data[p] = v;
-		this.data[p+1] = v;
-		this.data[p+2] = v;
-		this.data[p+3] = 255;
+		this.ctx.putImageData(this.imgdata, 0, 0);	
 	}
-}
+};
+
+JSRorschach.prototype.putPixel = function(x, y, v) {
+	var p = (x + y * this.width) << 2;
+	var data = this.data;
+	data[p] = v;
+	data[p+1] = v;
+	data[p+2] = v;
+	data[p+3] = 255;
+};
+
+JSRorschach.prototype.start = function(callback) {
+	var self = this;
+	var _step = function() {
+		self.step();
+		if(callback)
+			callback();
+		if(!self.paused) {
+			requestAnimationFrame( function() { _step() } );
+		}
+	}
+	_step();
+};
+
+JSRorschach.prototype.play = function(callback) {
+	this.paused = false;
+	this.start(callback);
+};
+
+JSRorschach.prototype.pause = function() {
+	this.paused = true;
+};
